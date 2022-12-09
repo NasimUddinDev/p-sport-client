a65b241eccebc3ae0f1407f1614e8a0d587bc398
MD-Baki <bakimd77@gmail.com>
2022-12-09 23:18:07 +0600

    add

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
-
 export default function Home() {
-  return (
-    <div data-theme="light" className="max-w-[1440px] mx-auto">
-      Hero
-    </div>
-  );
+    return (
+        <div data-theme="mytheme" className="w-full h-[2000px]">
+            <div className="max-w-[1440px] mx-auto">Hero kfjdslf</div>
+        </div>
+    );
 }
