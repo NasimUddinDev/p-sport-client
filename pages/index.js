d5e31a34fd7945b0aa1e53e02daf85df3f294462
MD-Baki <bakimd77@gmail.com>
2022-12-10 03:58:23 +0600

    football data add

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import FootballSchedule from "../components/FootballSchedule/FootballSchedule";
+
 export default function Home() {
     return (
         <div data-theme="mytheme" className="w-full h-[2000px]">
-            <div className="max-w-[1440px] mx-auto">Hero kfjdslf</div>
+            <div className="max-w-[1440px] mx-auto">
+                <FootballSchedule />
+            </div>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#460065",
-          secondary: "#550065",
-          third: "#020F2A",
-          accent: "#37cdbe",
-          neutral: "#3d4451",
-          "base-100": "#ffffff",
-        },
-      },
-      "dark",
-      "cupcake",
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
     ],
-  },
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#460065",
+                    secondary: "#550065",
+                    third: "#020F2A",
+                    accent: "#37cdbe",
+                    neutral: "#3d4451",
+                    "base-100": "#fff",
+                },
+            },
+            "dark",
+            "cupcake",
+        ],
+    },
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
 };
