d1769c1b070d67d02fd6c55c4fcef2133ba07629
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-12-09 23:17:33 +0600

    edited theme

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div data-theme="light">
+    <div data-theme="mytheme" className="w-full h-[2000px]">
       <div className="max-w-[1440px] mx-auto">Hero</div>
     </div>
   );
