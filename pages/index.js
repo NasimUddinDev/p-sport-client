73ae4061dc8ca79c5c4e7882009f52a8ad0647de
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-12-10 22:09:27 +0600

    nav done

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,10 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div data-theme="mytheme" className="w-full h-[2000px]">
-      <div className="max-w-[1440px] mx-auto">Hero</div>
+    <div data-theme="mytheme" className="w-full">
+      <div className="max-w-[1440px] mx-auto">
+        
+      </div>
     </div>
   );
 }
