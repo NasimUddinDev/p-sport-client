1ae9b89d7dcfc14d568b9487479a5e66698c57c8
MD-Baki <bakimd77@gmail.com>
2022-12-10 23:14:36 +0600

    football data add

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,8 @@ import FootballSchedule from "../components/FootballSchedule/FootballSchedule";
 
 export default function Home() {
     return (
-        <div data-theme="mytheme" className="w-full h-[2000px]">
-            <div className="max-w-[1440px] mx-auto">
-                <FootballSchedule />
-            </div>
+        <div data-theme="mytheme" className="w-full">
+            <div className="max-w-[1440px] mx-auto"></div>
         </div>
     );
 }
