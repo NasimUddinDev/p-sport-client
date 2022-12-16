fa43db19f2ae351427869e0c48fa8f01d7b9c365
nasim0994 <nasimuddin0994@gmail.com>
2022-12-16 22:42:42 +0600

    shudele card style change

--- components/Shedules/Shedules.js
+++ components/Shedules/Shedules.js
@@ -11,22 +11,22 @@ const Shedules = () => {
   }, []);
 
   return (
-    <div className="w-[80%] mx-auto">
+    <div className="">
       {isLoading && <h2>Loadding...</h2>}
 
       <div className="py-10">
-        <h2 className="text-center font-bold text-2xl border border-secondary mb-2 rounded-md m-0 py-2">
+        <h2 className="text-center font-bold text-2xl mb-2">
           Cricket Match- 0{shedule.length}
         </h2>
-        <div className="grid md:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {shedule &&
             shedule.map((match) => (
               <div
                 key={match._id}
-                className="border border-secondary rounded-md p-10"
+                className="border border-secondary rounded-md p-10 bg-gradient-to-r from-secondary via-primary to-secondary text-white"
               >
                 <div className="flex justify-between">
-                  <h4 className="uppercase font-bold text-secondary">
+                  <h4 className="uppercase font-bold text-white">
                     ICC- {match.matchs}
                   </h4>
                   <p className="font-bold">
@@ -35,13 +35,13 @@ const Shedules = () => {
                 </div>
                 <div className="flex justify-between pt-7">
                   <div className="flex gap-4 items-center">
-                    <img src={match.team1Flag} alt="" className="w-10 h-10" />
+                    <img src={match.team1Flag} alt="" className="w-10 h-6" />
                     <p className="font-bold capitalize">{match.team1}</p>
                   </div>
                   <p className="text-2xl font-bold text-orange-600">VS</p>
-                  <div className="flex gap-4 items-center">
+                  <div className="flex gap-2 items-center">
                     <p className="font-bold capitalize">{match.team2}</p>
-                    <img src={match.team2Flag} alt="" className="w-10 h-10" />
+                    <img src={match.team2Flag} alt="" className="w-10 h-6" />
                   </div>
                 </div>
               </div>
